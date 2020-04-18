c706f6f559121d5e75a4a18eed53fa887b0b4730
JaydenCole-55 <jaydenc@sfu.ca>
2020-04-18 13:57:41 -0700

    Square Numbers are calculated (1-64)

--- chessClass.py
+++ chessClass.py
@@ -2,53 +2,67 @@ import math
 
 class Piece():
     def __init__ (self, team, position, image):
-        self.team      = team
-        self.image     = image
-        self.col       = {'a':0, 'b':100, 'c':200, 'd':300, 'e':400, 'f':500, 'g':600, 'h':700}
-        self.row       = {'8':0, '7':100, '6':200, '5':300, '4':400, '3':500, '2':600, '1':700}
-        self.position  = position
+        self.team           = team
+        self.image          = image
+        self.rect           = None
+        self.name           = None
+        self.squareNum      = None
+        self.chessPosition  = None
+        self.screenPosition = None
+        self.col            = {'a':0, 'b':100, 'c':200, 'd':300, 'e':400, 'f':500, 'g':600, 'h':700}
+        self.row            = {'8':0, '7':100, '6':200, '5':300, '4':400, '3':500, '2':600, '1':700}
         self.fn_update_position( position )
-        self.rect      = None
-        self.name      = None
 
     def fn_update_position(self, position):
+        # Passed: Some form of position coordinates
+        # Returns: None
+        # Determines the location of the piece wtihin 1-64 and then calculates the screen position and 
+        # chess board location
         if isinstance(position, str):
-            self.fn_screenPosition( position )
-            self.position = position
+            # Turns chess string into screen coord tuple, then call itself using the tuple
+            x = self.col[position[0]]
+            y = self.row[position[1]]
+            self.fn_update_position( (x,y) )
         elif isinstance(position, tuple):
-            self.screenPos = self.fn_allign_position( position )
-            self.fn_update_chessboard_location( position )
+            # Update what square number its on
+            x = position[0]
+            y = position[1]
+            self.squareNum = 64 - 8 * (math.floor( y/100 )+1) + math.floor( x/100 ) + 1
+        elif isinstance( position, int ):
+            self.squareNum = position
         else:
             # ERROR Position passed to update Position was not str or tuple
             print("ERROR: " + piece.name + " did not update it's position porperly.")
 
-    def fn_screenPosition( self, chessPosition ):
-        x = self.col[chessPosition[0]]
-        y = self.row[chessPosition[1]]
-        self.screenPos = (x,y)
+        # Update the other methods of displaying position - based on the square number
+        self.fn_chessboard_location()
+        self.fn_screen_position()
+        
 
-    def fn_update_chessboard_location(self, xypos):
-        chess_columns = { 0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f', 6:'g', 7:'h' }
+    def fn_chessboard_location( self ):
+        # Passed: None
+        # Returns: None
+        # Updates piece's chess location based on its square number
+        chessColumns = { 1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 0:'h' }
     
-        col = chess_columns[ math.floor( xypos[0] / 100 ) ]
-        row = 7 - math.floor( xypos[1] / 100 ) + 1
+        col = chessColumns[ (self.squareNum % 8) ]
+        row = math.ceil( self.squareNum / 8 )
         chessLoc = str(col) + str(row)
-        self.position = chessLoc
+        self.chessPosition = chessLoc
         self.fn_update_name()
+
+    def fn_screen_position( self ):
+        x = ( ( self.squareNum % 8 ) - 1) * 100
+        if x < 0:
+            x = 700
+        y = ( 8 - math.ceil( self.squareNum / 8 ) ) * 100
+        self.screenPosition = (x, y)
 
     def fn_possible_moves( self ):
         pass
 
     def fn_update_name( self ):
         pass
-
-    def fn_allign_position(self, xypos):
-        # Passed:  tuple of screen coords
-        # Returns: coords of the top left pixel of that chess square
-
-        col = math.floor( xypos[0] / 100 ) * 100
-        row = math.floor( xypos[1] / 100 ) * 100
-        return (col,row)
 
 class King( Piece ):
     def __init__( self, team, position, image):
@@ -93,7 +107,7 @@ class Bishop( Piece ):
 class Pawn( Piece ):
     def __init__( self, team, position, image ):
         Piece.__init__(self, team, position, image )
-        self.name = self.team + "Pawn" + self.position
+        self.fn_update_name()
 
     def fn_posible_moves( self ):
         pass
@@ -102,4 +116,4 @@ class Pawn( Piece ):
         pass
 
     def fn_update_name( self ):
-        self.name = self.team + "Pawn" + self.position
+        self.name = self.team + "Pawn" + self.chessPosition
--- chessGame.py
+++ chessGame.py
@@ -10,15 +10,15 @@ from chessClass import *
 # Initialize the screen
 pygame.init()
 width, height = 800, 800
-screen=pygame.display.set_mode((width, height))
+screen = pygame.display.set_mode( ( width, height ) )
 pygame.mixer.init()
 
 # Initialize pathing
-current_path = os.path.dirname(__file__)
-resource_path = os.path.join(current_path, 'resources2')
-image_path = os.path.join(resource_path, 'images')
-audio_path = os.path.join(resource_path, 'audio')
-pieces_img_path = os.path.join(image_path, "pieces")
+current_path    = os.path.dirname( __file__ )
+resource_path   = os.path.join( current_path,  'resources2' )
+image_path      = os.path.join( resource_path, 'images'     )
+audio_path      = os.path.join( resource_path, 'audio'      )
+pieces_img_path = os.path.join( image_path,    'pieces'     )
 
 # Load audio
 pygame.mixer.music.load( os.path.join( audio_path, "intro.mp3" ) )
@@ -45,146 +45,153 @@ BKnightImg = pygame.image.load( os.path.join( pieces_img_path, "BKnight.png"))
 BBishopImg = pygame.image.load( os.path.join( pieces_img_path, "BBishop.png"))
 BPawnImg   = pygame.image.load( os.path.join( pieces_img_path, "BPawn.png"))
 
-# Start main menu
-main_menu = 0
-while main_menu:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-        
-        if event.type==pygame.MOUSEBUTTONDOWN and event.button == 1:
-            pos = pygame.mouse.get_pos()
-            if start_button_rect.collidepoint(pos):
-                main_menu = 0
-                play = 1
-            if quit_button_rect.collidepoint(pos):
-                sys.exit()
-
-    # Clear screen before drawing it again
-    screen.fill(0)
-
-    start_button_rect = screen.blit(start_button, ( int(width/2-128), int(height/2-200) ))
-    quit_button_rect  = screen.blit(quit_button,  ( int(width/2-128), int(height/2+56)  ))
-
-    pygame.display.flip()
-
-# Get Game Ready
-# Initialize pieces
-WKing    = King   ( 'W', 'e1', WKingImg   )
-WQueen   = Queen  ( 'W', 'd1', WQueenImg  )
-WRook1   = Rook   ( 'W', 'a1', WRookImg   )
-WRook2   = Rook   ( 'W', 'h1', WRookImg   )
-WKnight1 = Knight ( 'W', 'b1', WKnightImg )
-WKnight2 = Knight ( 'W', 'g1', WKnightImg )
-WBishop1 = Bishop ( 'W', 'c1', WBishopImg )
-WBishop2 = Bishop ( 'W', 'f1', WBishopImg )
-
-BKing    = King   ( 'B', 'e8', BKingImg   )
-BQueen   = Queen  ( 'B', 'd8', BQueenImg  )
-BRook1   = Rook   ( 'B', 'a8', BRookImg   )
-BRook2   = Rook   ( 'B', 'h8', BRookImg   )
-BKnight1 = Knight ( 'B', 'b8', BKnightImg )
-BKnight2 = Knight ( 'B', 'g8', BKnightImg )
-BBishop1 = Bishop ( 'B', 'c8', BBishopImg )
-BBishop2 = Bishop ( 'B', 'f8', BBishopImg )
-
-# Consolidate the pieces
-pieces = [ WKing, WQueen, WRook1, WRook2, WKnight1, WKnight2, WBishop1, WBishop2 ]
-pieces = pieces + [ BKing, BQueen, BRook1, BRook2, BKnight1, BKnight2, BBishop1, BBishop2 ]
-
-# Create the pawns
-col = { 0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f', 6:'g', 7:'h' }
-for pawnNum in range(16):
-    if pawnNum < 8:
-        team = 'W'
-        img = WPawnImg
-        row = '2'
-    else:
-        team = 'B'
-        img = BPawnImg
-        row = '7'
-    
-    pos = str(col[pawnNum % 8]) + str(row)
-    pawn = Pawn(team, pos, img)  
-    pieces = pieces + [pawn]
-
-# Pieces are ready, start game
-turn_num = -1 # Turn -1 initializes board
-while 1:
-    # Clear Screen
-    screen.fill(0)
-
-    # Determine which player turn
-    if turn_num%2 == 0:
-        player = 'W'
-        print("White turn")
-    else:
-        player = 'B'
-        print("Black turn")
-    
-    # Draw the board
-    for x in range(math.floor( width/ black_tile.get_width() ) + 6):
-        for y in range( math.floor( height/ black_tile.get_height() ) + 6):
-            if (x+y)%2 == 0:
-                screen.blit(white_tile,(x*100,y*100))
-            else:
-                screen.blit(black_tile,(x*100,y*100))
-
-    # First time through loop only draw the pieces, no turns
-    turn_going = True
-    if turn_num < 0:
-        turn_going = False
-
-    # Wait for user to choose where to move piece
-    chosen_piece = None  
-    while turn_going:
+def main():
+    # Start main menu
+    main_menu = 0
+    while main_menu:
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+
+            if event.type==pygame.MOUSEBUTTONDOWN and event.button == 1:
                 pos = pygame.mouse.get_pos()
-                if chosen_piece != None:
-                    # Check if it takes an opponents piece
-                    fn_check_if_takes( pos, pieces, player )
+                if start_button_rect.collidepoint( pos ):
+                    main_menu = 0
+                    play = 1
+                if quit_button_rect.collidepoint( pos ):
+                    sys.exit()
 
-                    # Then move piece to that position
-                    chosen_piece.rect.center = pos
-                    chosen_piece.fn_update_position( pos )
-                    pieces.append( chosen_piece )
-                    turn_going = False
+        # Clear screen before drawing it again
+        screen.fill(0)
+
+        start_button_rect = screen.blit( start_button, ( int(width/2-128), int(height/2-200 ) ) )
+        quit_button_rect  = screen.blit( quit_button,  ( int(width/2-128), int(height/2+56  ) ) )
+
+        pygame.display.flip()
+
+    # Get Game Ready
+    # Initialize pieces
+    WKing    = King   ( 'W', 'e1', WKingImg   )
+    WQueen   = Queen  ( 'W', 'd1', WQueenImg  )
+    WRook1   = Rook   ( 'W', 'a1', WRookImg   )
+    WRook2   = Rook   ( 'W', 'h1', WRookImg   )
+    WKnight1 = Knight ( 'W', 'b1', WKnightImg )
+    WKnight2 = Knight ( 'W', 'g1', WKnightImg )
+    WBishop1 = Bishop ( 'W', 'c1', WBishopImg )
+    WBishop2 = Bishop ( 'W', 'f1', WBishopImg )
+
+    BKing    = King   ( 'B', 'e8', BKingImg   )
+    BQueen   = Queen  ( 'B', 'd8', BQueenImg  )
+    BRook1   = Rook   ( 'B', 'a8', BRookImg   )
+    BRook2   = Rook   ( 'B', 'h8', BRookImg   )
+    BKnight1 = Knight ( 'B', 'b8', BKnightImg )
+    BKnight2 = Knight ( 'B', 'g8', BKnightImg )
+    BBishop1 = Bishop ( 'B', 'c8', BBishopImg )
+    BBishop2 = Bishop ( 'B', 'f8', BBishopImg )
+
+    # Consolidate the pieces
+    pieces = [ WKing, WQueen, WRook1, WRook2, WKnight1, WKnight2, WBishop1, WBishop2 ]
+    pieces = pieces + [ BKing, BQueen, BRook1, BRook2, BKnight1, BKnight2, BBishop1, BBishop2 ]
+
+    # Create the pawns
+    col = { 0:'a', 1:'b', 2:'c', 3:'d', 4:'e', 5:'f', 6:'g', 7:'h' }
+    for pawnNum in range( 16 ):
+        if pawnNum < 8:
+            team = 'W'
+            img = WPawnImg
+            row = '2'
+        else:
+            team = 'B'
+            img = BPawnImg
+            row = '7'
+
+        pos = str( col[pawnNum % 8] ) + str( row )
+        pawn = Pawn(team, pos, img)
+        pieces = pieces + [pawn]
+
+    # Pieces are ready, start game
+    turn_num = -1 # Turn -1 initializes board
+    while 1:
+        # Clear Screen
+        screen.fill(0)
+
+        # Determine which player turn
+        if turn_num%2 == 0:
+            player = 'W'
+            #print("White turn")
+        else:
+            player = 'B'
+            #print("Black turn")
+
+        # Draw the board
+        for x in range(math.floor( width/ black_tile.get_width() ) + 6):
+            for y in range( math.floor( height/ black_tile.get_height() ) + 6):
+                if (x+y)%2 == 0:
+                    screen.blit(white_tile,(x*100,y*100))
                 else:
-                    for piece in pieces:
-                        if piece.rect.collidepoint( pos ):
-                            # Player is attempting to move this piece
-                            if piece.team == player:
-                                # Remove from pieces list to be modified and appended back later
-                                chosen_piece = pieces.pop( pieces.index( piece ) )
-                                break
-            # Option to right click to deselect piece
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
-                # Put piece back in pieces list
-                pieces.append( chosen_piece )
-                chosen_piece = None
+                    screen.blit(black_tile,(x*100,y*100))
+
+        # First time through loop only draw the pieces, no turns
+        turn_going = True
+        if turn_num < 0:
+            turn_going = False
+
+        # Wait for user to choose where to move piece
+        chosen_piece = None  
+        while turn_going:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    pos = pygame.mouse.get_pos()
+                    if chosen_piece != None:
+                        #TODO heck if it is the same position -> to deselect
+
+                        # Check if it takes an opponents piece
+                        fn_check_if_takes( pos, pieces, player )
+
+                        # Then move piece to that position
+                        chosen_piece.rect.center = pos
+                        chosen_piece.fn_update_position( pos )
+                        pieces.append( chosen_piece )
+                        turn_going = False
+                    else:
+                        for piece in pieces:
+                            if piece.rect.collidepoint( pos ):
+                                # Player is attempting to move this piece
+                                if piece.team == player:
+                                    # Remove from pieces list to be modified and appended back later
+                                    chosen_piece = pieces.pop( pieces.index( piece ) )
+                                    break
+                # Option to right click to deselect piece
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                    # Put piece back in pieces list
+                    pieces.append( chosen_piece )
+                    chosen_piece = None
+
+        # Check its a possible move and not outside/around the board
+        # Check if it takes any piece
+        # Check if it puts their king in danger
+        # Check if it puts the other king in danger
+        # End turn, switch turns
+
+        # Draw the pieces
+        for piece2 in pieces:
+            piece2.rect = screen.blit( piece2.image, piece2.screenPosition )
+            print("I am " + piece2.name + " located at squareNum " + str(piece2.squareNum) + " with Chess: " + piece2.chessPosition + " and Screen: " + str(piece2.screenPosition[0]) + ", " +str(piece2.screenPosition[1]))
+
+        # Put the display to screen
+        pygame.display.flip()
+        turn_num = turn_num + 1
+
+def fn_check_if_takes( pos, pieces, player ):
+    # Passed: cords fo the position, list of pieces, the player colour
+    # Returns: None
+    # Checks if the passed square has an oppenent piece and removes it
+    for piece in pieces:
+        if piece.rect.collidepoint(pos):
+            if piece.team != player:
+                pieces.pop( pieces.index( piece ) )
+                return
+
+if __name__ == "__main__":
+    main()
     
-    # Check its a possible move and not outside/around the board
-    # Check if it takes any piece
-    # Check if it puts their king in danger
-    # Check if it puts the other king in danger
-    # End turn, switch turns
-
-    # Draw the pieces
-    for piece2 in pieces:
-        piece2.rect = screen.blit(piece2.image,piece2.screenPos)
-        #print("I am a " + piece2.name + " located at: " + piece2.position + ". x = " + str(piece2.screenPos[0]) + ", y = " + str(piece2.screenPos[1]) )
-
-    #print("\n=======================\n")
-    pygame.display.flip()
-    turn_num = turn_num + 1
-
-    def fn_check_if_takes( pos, pieces, player ):
-        # Passed: cords fo the position, list of pieces, the player colour
-        # Returns: None
-        # Checks if the passed square has an oppenent piece and removes it
-        for piece in pieces:
-            if piece.rect.collidepoint(pos):
-                if piece.team != player:
-                    pieces.pop( pieces.index( piece ) )
-            # Check if the pos is overlapping one of the pieces, if so, remove it
